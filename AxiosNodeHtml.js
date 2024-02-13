5917c405c82fd11e582d2f14ff471042f0cf9eb1
mathee24 <s6506021620156@email.kmutnb.ac.th>
2024-02-13 10:38:30 +0700

    .

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -9,7 +9,8 @@ const app = express();
 
 //Base URL for the API
 //const base_url ="https://api.example.com";
-const base_url = "http://node58261-env-7269086.proen.app.ruk-com.cloud";
+const base_url ="http://localhost5500"
+//const base_url = "http://node58261-env-7269086.proen.app.ruk-com.cloud";
 
 //Set the template engine
 app.set("views",path.join(__dirname,"/public/views"));
